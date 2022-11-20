0f7ba9cb76b83be8a086748e0f6b6e45d75447b7
Kabishan <ksuvendran@gmail.com>
2022-11-19 21:33:23 -0500

    Added logic for login validation

--- js/login.js
+++ js/login.js
@@ -1,3 +1,6 @@
+const USERNAME = "4hc3_ta";
+const PASSWORD = "4hc3";
+
 function showHorizontalPrototypeMessage() {
 	alert("This feature was not implemented vertically.");
 }
@@ -8,5 +11,36 @@ function showPassword() {
 		password.type = "text";
 	} else {
 		password.type = "password";
+	}
+}
+
+function validateLogin() {
+	var username = document.getElementById("username");
+	var password = document.getElementById("password");
+
+	var invalidUsernameFeedback = document.getElementById("invalid-username-feedback");
+	var invalidPasswordFeedback = document.getElementById("invalid-password-feedback");
+
+	invalidUsernameFeedback.style.display = "block";
+	invalidPasswordFeedback.style.display = "block";
+
+	if (username.value.length === 0) {
+		invalidUsernameFeedback.innerText = "Username cannot be left empty"
+	} 
+	else if (username.value === USERNAME) {
+		invalidUsernameFeedback.style.display = "none";
+	}
+	else {
+		invalidUsernameFeedback.innerText = "Username is not correct"
+	}
+
+	if (password.value.length === 0) {
+		invalidPasswordFeedback.innerText = "Password cannot be left empty"
+	} 
+	else if (password.value === PASSWORD) {
+		invalidPasswordFeedback.style.display = "none";
+	}
+	else {
+		invalidPasswordFeedback.innerText = "Password is not correct"
 	}
 }
