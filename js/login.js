e75162af7ce801a14df03312c45b6e14cdf38e56
Kabishan <ksuvendran@gmail.com>
2022-11-19 22:25:21 -0500

    Added more login logic

--- js/login.js
+++ js/login.js
@@ -14,9 +14,10 @@ function showPassword() {
 	}
 }
 
-function validateLogin() {
+async function validateLogin() {
 	var username = document.getElementById("username");
 	var password = document.getElementById("password");
+	var valid = true;
 
 	var invalidUsernameFeedback = document.getElementById("invalid-username-feedback");
 	var invalidPasswordFeedback = document.getElementById("invalid-password-feedback");
@@ -27,6 +28,7 @@ function validateLogin() {
 	if (username.value.length === 0) {
 		invalidUsernameFeedback.innerText = "Username cannot be left empty"
 		username.style.border = "2px solid red";
+		valid = valid && false;
 	} 
 	else if (username.value === USERNAME) {
 		invalidUsernameFeedback.style.display = "none";
@@ -35,11 +37,13 @@ function validateLogin() {
 	else {
 		invalidUsernameFeedback.innerText = "Username is not correct"
 		username.style.border = "2px solid red";
+		valid = valid && false;
 	}
 
 	if (password.value.length === 0) {
 		invalidPasswordFeedback.innerText = "Password cannot be left empty"
 		password.style.border = "2px solid red";
+		valid = valid && false;
 	} 
 	else if (password.value === PASSWORD) {
 		invalidPasswordFeedback.style.display = "none";
@@ -48,5 +52,25 @@ function validateLogin() {
 	else {
 		invalidPasswordFeedback.innerText = "Password is not correct"
 		password.style.border = "2px solid red";
+		valid = valid && false;
 	}
+
+	if (valid) {
+		var loginButton = document.getElementById("login-button");
+		var loginButtonParent = loginButton.parentNode;
+
+		var loginMessage = document.createElement("div");
+		loginMessage.style.setProperty("color", "green");
+		loginMessage.style.setProperty("text-align", "center");
+		loginMessage.innerText = "Correct Username and Password. Logging you in..."
+
+		loginButtonParent.insertBefore(loginMessage, loginButton);
+
+		await sleep(3000);
+		alert("Hello");
+	}
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
