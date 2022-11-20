a173795781efdbeca65a907ef4ec296390b1b3c6
Kabishan <ksuvendran@gmail.com>
2022-11-19 20:27:05 -0500

    Added Show Password feature

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
 function showHorizontalPrototypeMessage() {
 	alert("This feature was not implemented vertically.");
 }
+
+function showPassword() {
+	var password = document.getElementById("password");
+	if (password.type === "password") {
+		password.type = "text";
+	} else {
+		password.type = "password";
+	}
+}
